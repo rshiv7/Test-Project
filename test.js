b781636ca138bb75a00380bf79341ef7f8522193
Raghav Shiv <Raghav@Raghavs-MacBook-Air.local>
2019-06-11 12:18:19 -0700

    Still testing

--- test.js
+++ test.js
@@ -13,6 +13,30 @@ $(document).ready(function() {
     // Initialize Firebase
     firebase.initializeApp(firebaseConfig);
     
+    var provider = new firebase.auth.GoogleAuthProvider();
+
+    firebase.auth().signInWithPopup(provider).then(function(result) {
+        // This gives you a Google Access Token. You can use it to access the Google API.
+        var token = result.credential.accessToken;
+        // The signed-in user info.
+        var user = result.user;
+        // ...
+      }).catch(function(error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // The email of the user's account used.
+        var email = error.email;
+        // The firebase.auth.AuthCredential type that was used.
+        var credential = error.credential;
+        // ...
+      });
+
+
+
+
+
+
     function signInWithFacebook(){
     
         var provider = new firebase.auth.FacebookAuthProvider();
