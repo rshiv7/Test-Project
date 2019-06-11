5d462e69cfc0323ab88cd408e3b388bc708512e0
Raghav Shiv <Raghav@Raghavs-MacBook-Air.local>
2019-06-11 14:03:43 -0700

    Google

--- test.js
+++ test.js
@@ -1,4 +1,4 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
     // Your web app's Firebase configuration
     var firebaseConfig = {
@@ -6,22 +6,22 @@ $(document).ready(function() {
         authDomain: "finfo-81f61.firebaseapp.com",
         databaseURL: "https://finfo-81f61.firebaseio.com",
         projectId: "finfo-81f61",
-        storageBucket: "",
+        storageBucket: "finfo-81f61.appspot.com",
         messagingSenderId: "606365655397",
         appId: "1:606365655397:web:75ced34361481613"
     };
     // Initialize Firebase
     firebase.initializeApp(firebaseConfig);
-    
+
     var provider = new firebase.auth.GoogleAuthProvider();
 
-    firebase.auth().signInWithPopup(provider).then(function(result) {
+    firebase.auth().signInWithPopup(provider).then(function (result) {
         // This gives you a Google Access Token. You can use it to access the Google API.
         var token = result.credential.accessToken;
         // The signed-in user info.
         var user = result.user;
         // ...
-      }).catch(function(error) {
+    }).catch(function (error) {
         // Handle Errors here.
         var errorCode = error.code;
         var errorMessage = error.message;
@@ -30,17 +30,17 @@ $(document).ready(function() {
         // The firebase.auth.AuthCredential type that was used.
         var credential = error.credential;
         // ...
-      });
+    });
 
 
 
 
 
 
-    function signInWithFacebook(){
-    
+    function signInWithFacebook() {
+
         var provider = new firebase.auth.FacebookAuthProvider();
-    
+
         firebase.auth().signInWithPopup(provider).then(function (result) {
             // This gives you a Facebook Access Token. You can use it to access the Facebook API.
             var token = result.credential.accessToken;
@@ -58,8 +58,8 @@ $(document).ready(function() {
             // ...
         });
     };
-    
-    
+
+
     $("#fb").on("click", signInWithFacebook)
 })
 
