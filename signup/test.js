8cf0fe2c622c43d81e95cba44b0bfb43cbb819fb
Raghav Shiv <Raghav@Raghavs-MacBook-Air.local>
2019-06-11 11:26:50 -0700

    Testing Deployed link

---
+++ signup/test.js
@@ -0,0 +1,43 @@
+$(document).ready(function() {
+
+    // Your web app's Firebase configuration
+    var firebaseConfig = {
+        apiKey: "AIzaSyC0zlbQ6ZqjtRgB0K2WNtTrxqyZOv_zqKo",
+        authDomain: "finfo-81f61.firebaseapp.com",
+        databaseURL: "https://finfo-81f61.firebaseio.com",
+        projectId: "finfo-81f61",
+        storageBucket: "",
+        messagingSenderId: "606365655397",
+        appId: "1:606365655397:web:75ced34361481613"
+    };
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+    
+    function signInWithFacebook(){
+    
+        var provider = new firebase.auth.FacebookAuthProvider();
+    
+        firebase.auth().signInWithPopup(provider).then(function (result) {
+            // This gives you a Facebook Access Token. You can use it to access the Facebook API.
+            var token = result.credential.accessToken;
+            // The signed-in user info.
+            var user = result.user;
+            // ...
+        }).catch(function (error) {
+            // Handle Errors here.
+            var errorCode = error.code;
+            var errorMessage = error.message;
+            // The email of the user's account used.
+            var email = error.email;
+            // The firebase.auth.AuthCredential type that was used.
+            var credential = error.credential;
+            // ...
+        });
+    };
+    
+    
+    $("#fb").on("click", signInWithFacebook)
+})
+
+
+
